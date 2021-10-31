e340effebe1655446f3dc64a3d95511a06f3689a
Lucas Zacchi <zacchilucasm@gmail.com>
2021-10-31 14:12:19 -0300

    Add layout with simple button to main window

--- sgi/sgi/main.py
+++ sgi/sgi/main.py
@@ -1,15 +1,16 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from main_window import MainWindow
 
+from main_window import MainWindow
 
 import main_window
 
 def app() -> None:
     app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
+    main_window = MainWindow()
+    main_window.show()
     sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
     app()
--- sgi/sgi/main_window.py
+++ sgi/sgi/main_window.py
@@ -1,12 +1,21 @@
-from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5 import QtGui, QtCore, QtWidgets
 
-
-class MainWindow(QtWidgets.QMainWindow):
+_DEBUG = True
+class MainWindow(QtWidgets.QWidget):
     def __init__(self, parent=None) -> None:
+        """Initializes Main Window, setting initial size and title"""
         super().__init__(parent)
         self.resize(500,500)
+        self.setWindowTitle('INE5420 SGI')
         self.build()
 
     def build(self) -> None:
-        self.setWindowTitle('INE5420 SGI')
-        self.setWindowIcon(QtGui.QIcon('icon.png'))
+        """Builds Main window, with menus, buttons and views"""
+        self.close_btn = QtWidgets.QPushButton(self)
+        self.close_btn.setObjectName('close_btn')
+        menu_layout = QtWidgets.QVBoxLayout()
+        menu_layout.addWidget(self.close_btn)
+        self.setLayout(menu_layout)
+
+        if _DEBUG:
+            print(self.children())
