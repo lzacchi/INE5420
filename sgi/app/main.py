6f91c85c5a2addf52165e253f0d3dba3e6f21ee9
Lucas Zacchi <zacchilucasm@gmail.com>
2021-11-03 00:35:12 -0300

    Add New Wireframe Window

---
+++ sgi/app/main.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from GUI.main_window import MainWindow
+
+def app() -> None:
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    w = QMainWindow()
+    ex.setupUi(w)
+    w.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    app()
--- sgi/math_utils/viewport_transformation.py
+++ sgi/math_utils/viewport_transformation.py
@@ -5,7 +5,7 @@ def x_viewport(x_window: int,
          xvp_max: int) -> float:
 
     """Return the x coordinate of the viewport transformation function"""
-    x_vp = (x_window - xwindow_min/xwindow_max - xwindow_min) * (xvp_max - xvp_min)
+    x_vp = ((x_window - xwindow_min)/xwindow_max - xwindow_min) * (xvp_max - xvp_min)
     return x_vp
 
 def y_viewprot(y_window: int,
@@ -15,5 +15,5 @@ def y_viewprot(y_window: int,
                yvp_max: int) -> float:
     """Return the y coordinate of the viewport transformation function"""
 
-    y_vp = (1 - (y_window - ywindow_min/ywindow_max - ywindow_min)) * (yvp_max - yvp_min)
+    y_vp = (1 - ((y_window - ywindow_min)/ywindow_max - ywindow_min)) * (yvp_max - yvp_min)
     return y_vp
